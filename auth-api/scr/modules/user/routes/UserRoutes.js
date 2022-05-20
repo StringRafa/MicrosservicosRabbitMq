be5ee00c7a8eddbc608950db9d8f49b315ef5b89
Rafael <rafa.jo.souza@hotmail.com>
2022-05-20 16:35:55 -0300

    Desenvolvendo a autenticacao com JWT

--- auth-api/scr/modules/user/routes/UserRoutes.js
+++ auth-api/scr/modules/user/routes/UserRoutes.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
 import UserController from "../controller/UserController.js";
-import User from "../model/User.js";
 
 const router = new Router();
 
 router.get("/api/user/email/:email", UserController.findByEmail);
+router.post("/api/user/auth", UserController.getAccessToken);
 
 export default router;
