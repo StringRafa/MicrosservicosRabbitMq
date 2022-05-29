2de7cc129e64925f8e79d82fa732b108ca3f7b3f
Rafael <rafa.jo.souza@hotmail.com>
2022-05-29 12:55:20 -0300

    feat:Criando metodos GET para Categoria

--- product-api/src/main/java/com/microsservice/productapi/repository/CategoryRepository.java
+++ product-api/src/main/java/com/microsservice/productapi/repository/CategoryRepository.java
@@ -1,11 +1,17 @@
 package com.microsservice.productapi.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.microsservice.productapi.model.Category;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long>{
 
+	@Transactional(readOnly = true)
+	List<Category> findByDescriptionIgnoreCaseContaining(String description);
+	
 }
--- product-api/src/main/java/com/microsservice/productapi/service/CategoryService.java
+++ product-api/src/main/java/com/microsservice/productapi/service/CategoryService.java
@@ -2,6 +2,9 @@ package com.microsservice.productapi.service;
 
 import static org.springframework.util.ObjectUtils.isEmpty;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,7 +20,33 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository categoryRepository;
 	
+	public CategoryResponse findByIdResponse(Long id) {
+		return CategoryResponse.of(findById(id));
+	}
+	
+	public List<CategoryResponse> findAll(){
+		return categoryRepository
+				.findAll()
+				.stream()
+				.map(CategoryResponse::of)//ou category -> CategoryResponse.of(category)
+				.collect(Collectors.toList());
+	}
+	
+	public List<CategoryResponse> findByDescription(String description){
+		if (isEmpty(description)) {
+			throw new ValidationException("The category description must be informed.");
+		}
+		return categoryRepository
+				.findByDescriptionIgnoreCaseContaining(description)
+				.stream()
+				.map(CategoryResponse::of)//ou category -> CategoryResponse.of(category)
+				.collect(Collectors.toList());
+	}
+	
 	public Category findById(Long id) {
+		if (isEmpty(id)) {
+			throw new ValidationException("The category ID was not informed.");
+		}
 		return categoryRepository
 				.findById(id)
 				.orElseThrow(() -> new ValidationException("There's no category for the given ID."));
